8920f66640b0ddc2c6232a075f3c2b9d1f66d572
Brian Anderson <brian@brians-mbp.home>
2013-10-27 18:56:45 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<link rel="stylesheet" href="panes.css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script src="panes.js"></script>
+</head>
+
+<body>
+<div class="row">
+	<div class="pane">
+		<div class="pane-info">
+			<h1>Content</h1>
+			<p>More content</p>
+		</div>
+		<img src=""/>
+	</div>
+</div>
+</body>
+</html>
---
+++ panes.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($){
+	$('.pane').hover(function(){
+		$('.pane-info').fadeOut();
+	});
+});
+
