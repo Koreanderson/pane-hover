7b5c272eeae950837529b2a6739584b91151509a
brian <koreanderson@gmail.com>
2014-06-24 11:25:35 -0400

    style changes

--- index.php
+++ index.php
@@ -6,64 +6,67 @@
 </head>
 
 <body>
-<div class="row">
-	<div class="pane pane-fade">
-		<div class="pane-info">
-			<h1>This is IU</h1>
-			<p>jQuery function</p>
+<div class="panels">
+	<div class="row">
+		<div class="pane pane-fade">
+			<div class="pane-info">
+				<h1>Fade In</h1>
+				<p>jQuery function</p>
+			</div>
+			<div>
+				<img src="assets/iu_thumb.jpg"/>
+			</div>
 		</div>
-		<div>
-			<img src="assets/iu_thumb.jpg"/>
-		</div>
-	</div>
 
-	<div class="pane pane-rotate">
-		<div class="pane-info">
-			<h1>This is IU</h1>
-			<p>CSS animation</p>
+		<div class="pane pane-rotate">
+			<div class="pane-info">
+				<h1>Rotate In</h1>
+				<p>CSS animation</p>
+			</div>
+			<div>
+				<img src="assets/iu_thumb.jpg"/>
+			</div>
 		</div>
-		<div>
-			<img src="assets/iu_thumb.jpg"/>
-		</div>
-	</div>
 
-	<div class="pane pane-grow">
-		<div class="pane-info">
-			<h1>This is IU</h1>
-			<p>CSS Animation</p>
+		<div class="pane pane-grow">
+			<div class="pane-info">
+				<h1>Corner Expand</h1>
+				<p>CSS Animation</p>
+			</div>
+			<div>
+				<img src="assets/iu_thumb.jpg"/>
+			</div>
 		</div>
-		<div>
-			<img src="assets/iu_thumb.jpg"/>
+	</div><!-- /.row -->
+	<div class="row">
+		<div class="pane pane-hslide">
+			<div class="pane-info">
+				<h1>Horizontal Slide</h1>
+				<p>CSS Animation</p>
+			</div>
+			<div>
+				<img src="assets/iu_thumb.jpg"/>
+			</div>
 		</div>
-	</div>
-
-	<div class="pane pane-hslide">
-		<div class="pane-info">
-			<h1>This is IU</h1>
-			<p>CSS Animation</p>
+		<div class="pane pane-vslide">
+			<div class="pane-info">
+				<h1>Vertical Slide</h1>
+				<p>CSS Animation</p>
+			</div>
+			<div>
+				<img src="assets/iu_thumb.jpg"/>
+			</div>
 		</div>
-		<div>
-			<img src="assets/iu_thumb.jpg"/>
+		<div class="pane pane-center">
+			<div class="pane-info">
+				<h1>Center Expand</h1>
+				<p>CSS Animation</p>
+			</div>
+			<div>
+				<img src="assets/iu_thumb.jpg"/>
+			</div>
 		</div>
-	</div>
-	<div class="pane pane-vslide">
-		<div class="pane-info">
-			<h1>This is IU</h1>
-			<p>CSS Animation</p>
-		</div>
-		<div>
-			<img src="assets/iu_thumb.jpg"/>
-		</div>
-	</div>
-	<div class="pane pane-center">
-		<div class="pane-info">
-			<h1>This is IU</h1>
-			<p>CSS Animation</p>
-		</div>
-		<div>
-			<img src="assets/iu_thumb.jpg"/>
-		</div>
-	</div>
-</div>
+	</div><!-- / .row -->
+</div> <!-- /.panels -->
 </body>
 </html>
